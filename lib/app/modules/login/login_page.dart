de95d560848f6be30e73a889653593be2865075e
joaovs <joaovscamargo01@gmail.com>
2021-04-04 17:30:23 -0300

    feat: [WIP] create login module

---
+++ lib/app/modules/login/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:taisen/app/modules/login/login_controller.dart';
+
+class LoginPage extends GetView<LoginController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('LoginPage')),
+        body: SafeArea(child: Text('LoginController')));
+  }
+}
